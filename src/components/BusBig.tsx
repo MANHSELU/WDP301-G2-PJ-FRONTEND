ee5bdd9e2ff2192888cf29da830cd09a2ff44b16
son090103 <144188086+son090103@users.noreply.github.com>
2026-01-18 14:10:42 +0700

    update code mới

--- src/components/BusBig.tsx
+++ src/components/BusBig.tsx
@@ -1,42 +1,32 @@
-export default function BustripLogoBg({
-    className,
-}: {
-    className?: string;
-}) {
+export default function BustripLogoBg(
+) {
     return (
-        <div
-            className={`
-        absolute
-        w-full
-        h-full
-        ${className}
-      `}
-        >
+        <div className="absolute top-0 right-0 w-[840px] h-[840px] opacity-20 z-30 pointer-events-none">
             <img
                 src="/images/vectorxe2.png"
                 className="absolute"
-                style={{ top: "2%", left: "35%", width: "48%", height: "22%" }}
+                style={{ top: "2%", left: "33%", width: "48%", height: "22%" }}
                 alt=""
             />
 
             <img
                 src="/images/vectorxe3.png"
                 className="absolute"
-                style={{ top: "10%", left: "33%", width: "50%", height: "65%" }}
+                style={{ top: "10%", left: "31%", width: "50%", height: "65%" }}
                 alt=""
             />
 
             <img
                 src="/images/veactorxe1.png"
                 className="absolute"
-                style={{ top: "13%", left: "20%", width: "15%", height: "58%" }}
+                style={{ top: "13%", left: "18%", width: "15%", height: "58%" }}
                 alt=""
             />
 
             <img
                 src="/images/vectorxe4.png"
                 className="absolute"
-                style={{ bottom: "19%", left: "35%", width: "48%", height: "23%" }}
+                style={{ bottom: "19%", left: "33%", width: "48%", height: "23%" }}
                 alt=""
             />
         </div>
--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -7,24 +7,13 @@ export default function AppLayout({
 }) {
     return (
         <div className="relative min-h-screen">
-            {/* DECOR BACKGROUND */}
-            <BustripLogoBg
-                className="
-    right-[0px]
-    top-[0px]
-    w-[850px]
-    h-[850px]
-    opacity-20
-    z-30
-    pointer-events-none
-  "
-            />
-
+            <BustripLogoBg />
 
             {/* CONTENT */}
             <div className="relative z-20">
                 {children}
             </div>
         </div>
+
     );
 }
